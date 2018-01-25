c2a711cb4efaf51928dc9b94ea209459693e038e
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 03:17:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length > 0
+    numbered_array = array.collect{|name| "#{array.index(name) + 1}. #{name}"}
+    puts "The line is currently: #{numbered_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
